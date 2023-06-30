beab9cb3b1b6cf8834fe457b5890b008b8eada76
JT <raikiri24@gmail.com>
2023-06-29 20:52:50 -0400

    added color validator

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const color = require('color-name')
+const color = require('color-name');
 
-userInput = [
+function validColor(input) {
+    const colorName = input.toLowerCase();
+    return color[colorName] !== undefined;
+};
+
+const userInput = [
     {
         name: 'text',
         message: 'Please enter THREE (3) characters.',
         validate: (input) => {
             if (input.length !== 3) {
                 return 'Please enter THREE (3) characters.'
-            } else { return true }
+            } return true;
         }
     },
     {
         name: 'textColor',
         message: 'Please select text color.',
-    }
-
-    ,
+        validate: (input) => {
+            if (!validColor(input)) {
+                return 'Please enter a valid Color Kayword or Hexadeciaml number.';
+            }
+            return true;
+        }
+    },
     {
         name: 'shape',
         message: 'Please choose a shape.',
-        Type: 'list',
+        type: 'list',
         choices: ['circle', 'triangle', 'square'],
     },
     {
         name: 'shapeColor',
         message: 'Please choose a shape color',
-        type: 'list',
-
+        validate: (input) => {
+            if (!validColor(input)) {
+                return 'Please enter a valid Color Kayword or Hexadeciaml number.';
+            }
+            return true;
+        }
     }
-
 ];
+
+inquirer.prompt(userInput).then((answers) => {
+    console.log(answers)
+});
